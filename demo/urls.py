e9c84c9c72fb4d68681dc398ef45e89d96ca5945
Nilay <nilaynishant@gmail.com>
2020-07-29 03:38:58 +0530

    Update Broker None

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r'^(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)', views.index, name='index1'),
+    # path('', views.index, name='index'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from demo.tasks import add
+from PIL import Image
 
+# class image:
+#     def __init__(self, a):
+#         ## private varibale or property in Python
+#         self.__a = a
 
-class image:
-    def __init__(self, a):
-        ## private varibale or property in Python
-        self.__a = a
+#     ## getter method to get the properties using an object
+#     def get_a(self):
+#         return self.__a
 
-    ## getter method to get the properties using an object
-    def get_a(self):
-        return self.__a
-
-    ## setter method to change the value 'a' using an object
-    def set_a(self, a):
-        self.__a = a
+#     ## setter method to change the value 'a' using an object
+#     def set_a(self, a):
+#         self.__a = a
 
 
 # Create your views here.
-def index(request):
+def index(request,z, x, y):
     result = add.delay(7, 8)
     print(result.get(timeout=1))
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # image = Image.fromarray(result.get(timeout=1))
+    # response = image.save('img/png')
+    img = Image.fromarray(result.get(timeout=1)).resize((256, 256), Image.NEAREST)
+    # serialize to HTTP response
+    response = HttpResponse(content_type="image/png")
+    img.save(response, "PNG")
+    return response
